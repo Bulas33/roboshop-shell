0a1d7666bbd4bd0425b3a287edf51f6709c4a7be
sunil <you@example.com>
2024-01-26 19:43:23 +0530

    roboshop shell script

--- mongodb.sh
+++ mongodb.sh
@@ -13,7 +13,8 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 echo "script started excuting at $TIMESTAMP" &>> $LOGFILE
 
-VALIDATE(){
+VALIDATE
+{
     if [$1 -ne 0]
     then
        echo -e "$2...$R FAILED $N"
